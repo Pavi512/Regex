7243e058c7ee5eb326be2004746f6e8b33bab9a8
Pavi512 <gondipavithra@gmail.com>
2026-07-16 10:44:37 +0530

    Last name validation

--- src/com/bridgelabz/regex/UserRegistrationForm.java
+++ src/com/bridgelabz/regex/UserRegistrationForm.java
@@ -7,11 +7,16 @@ import java.util.regex.Pattern;
 public class UserRegistrationForm {
     static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        String firstName = sc.nextLine();
+        //Taking user input
+        String lastName = sc.nextLine();
 
+        //Regex to validate last name
         Pattern fn = Pattern.compile("^[A-Z][a-z]{2,}$");
-        Matcher m = fn.matcher(firstName);
 
+        //Matcher to check whether the last name is matches with the regex
+        Matcher m = fn.matcher(lastName);
+
+        //Prints true if matches otherwise false
         System.out.println(m.matches());
     }
 }
