f7aa6c91c208b1d55e52a2c70a862d51158085b7
Pavi512 <gondipavithra@gmail.com>
2026-07-16 10:49:55 +0530

    Email validation

--- src/com/bridgelabz/regex/UserRegistrationForm.java
+++ src/com/bridgelabz/regex/UserRegistrationForm.java
@@ -8,13 +8,13 @@ public class UserRegistrationForm {
     static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         //Taking user input
-        String lastName = sc.nextLine();
+        String email = sc.nextLine();
 
-        //Regex to validate last name
-        Pattern fn = Pattern.compile("^[A-Z][a-z]{2,}$");
+        //Regex to validate email
+        Pattern fn = Pattern.compile("^[A-Za-z0-9_.-]+@[A-Za-z_.-]+\\.[a-z]{2,}$");
 
-        //Matcher to check whether the last name is matches with the regex
-        Matcher m = fn.matcher(lastName);
+        //Matcher to check whether the email is matches with the regex
+        Matcher m = fn.matcher(email);
 
         //Prints true if matches otherwise false
         System.out.println(m.matches());
