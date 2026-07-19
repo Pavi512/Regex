2083c6a9c5ba38839c9e44154251bf4297cedb83
Pavi512 <gondipavithra@gmail.com>
2026-07-19 19:54:02 +0530

    [Pavithra] : User registration form validation

--- src/com/bridgelabz/regex/UserRegistrationForm.java
+++ src/com/bridgelabz/regex/UserRegistrationForm.java
@@ -2,29 +2,52 @@ package com.bridgelabz.regex;
 
 import java.util.regex.Pattern;
 
+// Custom Exception Class
+class UserRegistrationException extends Exception {
+    public UserRegistrationException(String message) {
+        super(message);
+    }
+}
+
 class ValidateUserRegistration {
-    //To validate FirstName
-    public boolean validateFirstName(String firstName) {
-        return Pattern.matches("^[A-Z][a-z]{3,}$", firstName);
+
+    //To validate FirstName if invalid throw exception
+    public boolean validateFirstName(String firstName) throws UserRegistrationException {
+        if (!Pattern.matches("^[A-Z][a-z]{3,}$", firstName)) {
+            throw new UserRegistrationException("Invalid First Name");
+        }
+        return true;
     }
 
-    //To validate LastName
-    public boolean validateLastName(String lastName) {
-        return Pattern.matches("^[A-Z][a-z]{3,}$", lastName);
+    //To validate LastName if invalid throw exception
+    public boolean validateLastName(String lastName) throws UserRegistrationException {
+        if (!Pattern.matches("^[A-Z][a-z]{3,}$", lastName)) {
+            throw new UserRegistrationException("Invalid Last Name");
+        }
+        return true;
     }
 
-    //To validate Email
-    public boolean validateEmail(String email) {
-        return Pattern.matches("^[A-Za-z0-9_.+-]+@[A-Za-z0-9_.-]+\\.[a-z]{2,}$", email);
+    //To validate Email if invalid throw exception
+    public boolean validateEmail(String email) throws UserRegistrationException {
+        if (!Pattern.matches("^[A-Za-z0-9_.+-]+@[A-Za-z0-9_.-]+\\.[a-z]{2,}$", email)) {
+            throw new UserRegistrationException("Invalid Email");
+        }
+        return true;
     }
 
-    //To validate Mobile
-    public boolean validateMobile(String mobile) {
-        return Pattern.matches("^91\\s[6-9][0-9]{9}$", mobile);
+    //To validate Mobile if invalid throw exception
+    public boolean validateMobile(String mobile) throws UserRegistrationException {
+        if (!Pattern.matches("^91\\s[6-9][0-9]{9}$", mobile)) {
+            throw new UserRegistrationException("Invalid Mobile Number");
+        }
+        return true;
     }
 
-    //To validate Password
-    public boolean validatePassword(String password) {
-        return Pattern.matches("^(?=.*[A-Z])+(?=.*[a-z])+(?=.*[0-9])+(?=.*[@#$&*+]).{8,}$", password);
+    //To validate Password if invalid throw exception
+    public boolean validatePassword(String password) throws UserRegistrationException {
+        if (!Pattern.matches("^(?=.*[A-Z])+(?=.*[a-z])+(?=.*[0-9])+(?=.*[@#$&*+]).{8,}$", password)) {
+            throw new UserRegistrationException("Invalid Password");
+        }
+        return true;
     }
 }
