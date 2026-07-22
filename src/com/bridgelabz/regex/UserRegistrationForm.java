2f5e8d795208f630a17c8db46229162f5bbaa147
Pavi512 <gondipavithra@gmail.com>
2026-07-22 22:20:56 +0530

    [Pavithra] : Implemented Lambda-based validation for user registration fields

--- src/com/bridgelabz/regex/UserRegistrationForm.java
+++ src/com/bridgelabz/regex/UserRegistrationForm.java
@@ -2,6 +2,12 @@ package com.bridgelabz.regex;
 
 import java.util.regex.Pattern;
 
+// Functional Interface
+@FunctionalInterface
+interface UserValidator {
+    boolean validate(String input) throws UserRegistrationException;
+}
+
 // Custom Exception Class
 class UserRegistrationException extends Exception {
     public UserRegistrationException(String message) {
@@ -11,43 +17,43 @@ class UserRegistrationException extends Exception {
 
 class ValidateUserRegistration {
 
-    //To validate FirstName if invalid throw exception
-    public boolean validateFirstName(String firstName) throws UserRegistrationException {
+    // Lambda to validate First Name if invalid throw exception
+    UserValidator firstNameValidator = firstName -> {
         if (!Pattern.matches("^[A-Z][a-z]{3,}$", firstName)) {
             throw new UserRegistrationException("Invalid First Name");
         }
         return true;
-    }
+    };
 
-    //To validate LastName if invalid throw exception
-    public boolean validateLastName(String lastName) throws UserRegistrationException {
+    //Lambda to  validate LastName if invalid throw exception
+    UserValidator lastNameValidator = lastName -> {
         if (!Pattern.matches("^[A-Z][a-z]{3,}$", lastName)) {
             throw new UserRegistrationException("Invalid Last Name");
         }
         return true;
-    }
+    };
 
-    //To validate Email if invalid throw exception
-    public boolean validateEmail(String email) throws UserRegistrationException {
+    //Lambda to  validate Email if invalid throw exception
+    UserValidator emailValidator = email -> {
         if (!Pattern.matches("^[A-Za-z0-9_.+-]+@[A-Za-z0-9_.-]+\\.[a-z]{2,}$", email)) {
             throw new UserRegistrationException("Invalid Email");
         }
         return true;
-    }
+    };
 
-    //To validate Mobile if invalid throw exception
-    public boolean validateMobile(String mobile) throws UserRegistrationException {
+    //Lambda to  validate Mobile if invalid throw exception
+    UserValidator mobileValidator = mobile -> {
         if (!Pattern.matches("^91\\s[6-9][0-9]{9}$", mobile)) {
             throw new UserRegistrationException("Invalid Mobile Number");
         }
         return true;
-    }
+    };
 
-    //To validate Password if invalid throw exception
-    public boolean validatePassword(String password) throws UserRegistrationException {
-        if (!Pattern.matches("^(?=.*[A-Z])+(?=.*[a-z])+(?=.*[0-9])+(?=.*[@#$&*+]).{8,}$", password)) {
+    //Lambda to  validate Password if invalid throw exception
+    UserValidator passwordValidator = password -> {
+        if (!Pattern.matches("^(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9])(?=.*[@#$&*+]).{8,}$", password)) {
             throw new UserRegistrationException("Invalid Password");
         }
         return true;
-    }
+    };
 }
